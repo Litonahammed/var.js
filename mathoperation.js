4cc7e97873fe4a5f78c53cd9cd64a02f0f33465e
apurba11221 <apurba11221@waltonbd.com>
2022-03-20 04:27:23 +0600

    first commit

---
+++ mathoperation.js
@@ -0,0 +1,8 @@
+var price1 = 31;
+var price2 = 29;
+var sum = price1 + price2;
+var defference = price1 - price2;
+
+price1 = price1+10;
+var multiplication = price1 * price2;
+console.log(multiplication);
---
+++ variable.js
@@ -0,0 +1,8 @@
+var onionPrice = 43;
+var eggPrice = 50;
+var totaPrice = onionPrice + eggPrice
+var priceDefferent = onionPrice - eggPrice
+// var eggQuantity = 7;
+var moneyAvailable = 500;
+var eggQuantity = moneyAvailable/eggPrice
+console.log(eggQuantity);
