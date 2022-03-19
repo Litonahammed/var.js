b3c1e8bc18a3b00edb81eccf6dbb0b4242b64c2b
apurba11221 <apurba11221@waltonbd.com>
2022-03-20 04:52:40 +0600

    short hand

---
+++ math-shorthand.js
@@ -0,0 +1,12 @@
+var price1 = 30;
+var price2 = 20;
+var sum = price1 + price2;
+var defference = price1 - price2;
+
+price1 = price1+10;
+price2 += 50;
+var age = 24;
+age = age +1 ;
+age += 1;
+age ++; 
+// This is short hand technic
