58edee9676bf4c99f7837fcbae3d35c4650d7337
apurba11221 <apurba11221@waltonbd.com>
2022-03-20 23:03:31 +0600

    parseInt

--- conversion.js
+++ conversion.js
@@ -1,4 +1,36 @@
-var firstName = 'Liton';
-var lastName = 'Ahammed';
-var fullName = firstName + lastName;
-console.log(fullName);
+
+
+ 
+// var firstName = 'Liton';
+// var lastName = 'Ahammed';
+// var fullName = firstName + lastName; 
+// var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+
+// // string
+// var onionPrice ="42";
+// var eggPrice = '41';
+// // var totalPrice = onionPrice + eggPrice ;
+// // console.log(totalPrice)
+
+// // // Integer
+// // var sunglass = 3;
+// // // Float
+// // var eggPrice = 99.99;
+
+// // string k Number a convert kore parseInt() diye
+// var onionPrice = '42';
+// var eggPrice = '41';
+// var onionPriceNumber = parseInt(onionPrice);
+// var eggPriceNumber = parseInt(eggPrice);
+// var totalPriceNumber = onionPriceNumber + eggPriceNumber;
+// console.log(totalPriceNumber);
+
+// parseFloat()
+var onionPrice = '42.50';
+var eggPrice = '41.33';
+var onionPriceNumber = parseFloat(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+var totalPriceNumber = onionPriceNumber + eggPriceNumber;
+console.log(totalPriceNumber);
